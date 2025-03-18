281d31a6b0ce2fc6c30a4e316fb62de6b7afbee2
temuritsutskiridze <temuri.tsutskiridze.04@gmail.com>
2025-03-19 01:03:08 +0400

    build header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>logo</h1>
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
